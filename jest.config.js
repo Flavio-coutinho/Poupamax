d4a539fcaade25ef49b4cae100ec3eba78fdb310
Flávio Coutinho <coutinhoflavio20@gmail.com>
2022-11-06 20:52:11 +0100

    test: change DB_URL var to avoid use same real db

---
+++ jest.config.js
@@ -0,0 +1,13 @@
+require('dotenv-safe/config')
+
+const { exec } = require('child_process')
+
+process.env.DB_URL = `${process.env.DB_URL}_testdb02?schema=test_schema`
+
+
+// TODO: transform in syncronous execution to avoid race conditions
+exec('yarn db:migrate')
+
+module.exports = {
+
+}
--- src/routes.test.js
+++ src/routes.test.js
@@ -1,12 +1,18 @@
 import request from 'supertest';
+import bcrypt from 'bcrypt';
+import { prisma } from '~/data'
 import { app } from './server-setup'
 
 const server = app.listen()
 
-describe('User routes',  () => {
+describe('User routes', () => {
+  beforeAll(async () => {
+    await prisma.user.deleteMany({})
+  })
+
   it('should return not found with wrong password', async () => {
     // Preparação
-    const email = 'coutinho@gmail.com';
+    const email = 'coutinho2@gmail.com';
     const password = 'teste';
 
     // execução
@@ -27,5 +33,29 @@ describe('User routes',  () => {
     // Expectação
     expect(result.status).toBe(404)
   });
+
+  it('should return logged in user by correct credentials', async () => {
+    // Preparação
+    const email = 'coutinho@gmail.com';
+    const password = '123456';
+
+    const saltRounds = 10
+    const hashedPassword = await bcrypt.hash(password, saltRounds)
+
+    await prisma.user.create({
+      data: { email, password: hashedPassword }
+    })
+
+    // execução
+    const result = await request(server).get('/login').auth(email, password)
+
+    // Expectação
+    expect(result.status).toBe(200)
+    expect(result.body.user).toBeTruthy()
+    expect(result.body.user.id).toBeTruthy()
+    expect(result.body.user.email).toBe(email)
+    // expect(result.body.user.password).toBeFalsy()
+  });
+
 
 });
